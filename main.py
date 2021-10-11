bea1b79732cea6b26f8afb0ad4af69af2c3af45c
asdfgzxqw1 <papayamoonand@gmail.com>
2021-10-11 21:08:13 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+name = "Monkey D. Dragon"
+def gtint ():
+    name = "Monkey D. Dragon"
+    digit = 43
+    list_of_digits = [1, 22, 2, 3, 4]
+    print(list_of_digits)
+
+
+
+if __name__ == '__main__':
+ print(name)
+ digit = 43
+ leist_of_digits = [1, 22, 2, 3, 4]
+print(leist_of_digits)
+print(int("993")-int("7"))
+print(int("2")<int("2"))
+print(int("3")>int("3"))
+print(int("2")<int("3"))
+print(int("3")>int("2"))
+
+
